d76ca6070ef777c738ad7d02c85a0d89576b2988
niels <niels@DESKTOP-9CU0PMO>
2017-02-13 14:04:08 +0100

    bootstrap begin

---
+++ function dropdown.php
@@ -0,0 +1,46 @@
+@@ -1,46 +0,0 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        
+        //Test Text
+        function studententen_dropdown() {
+
+            $servername = "10.25.222.12";
+            $username = "slb";
+            $password = "SjaakAfhaal000";
+            $dbname = "myDB";
+
+// Create connection
+            $conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+
+            $sql = "SELECT id, firstname, lastname FROM MyGuests";
+            $result = $conn->query($sql);
+
+            if ($result->num_rows > 0) {
+                // output data of each row
+                while ($row = $result->fetch_assoc()) {
+                    echo "id: " . $row["id"] . " - Name: " . $row["firstname"] . " " . $row["lastname"] . "<br>";
+                }
+            } else {
+                echo "0 results";
+            }
+            $conn->close();
+        }
+        ?>
+    </body>
+</html>
