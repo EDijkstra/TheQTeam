c60131f438207ccfc836da52ac8b000decfb8414
DESKTOP-B87CD5K\Paco-pc <pablo_nicolaas@live.nl>
2017-02-17 14:59:10 +0100

    Top 5 werkt nu moet alleen in de layout worden aangeroepen

--- GridTop5.php
+++ GridTop5.php
@@ -1,15 +1,46 @@
 <?php
 $sHost = 'localhost';
-$sUser = '';
+$sUser = 'root';
 $sPass = '';
+$sDB = 'slb';
 
-$conStr = mysqli_connect($sHost, $sUser, $sPass);
+//create connection
+$conStr = mysqli_connect($sHost, $sUser, $sPass, $sDB);
 
+// check connection
 if (!( $conStr )) {
     die('Failed to connect to MySQL Database Server - #' . mysqli_connect_errno() . ': ' . mysqli_Connect_error());
     if (!mysqli_select_db('slb')) {
         die('Connected to Server, but Failed to Connect to Database - #' . mysqli_connect_errno() . ': ' . mysqli_connect_errno());
     }
 }
-else{echo " It works!!!";}
-$sql = '';
+else{echo " It works!!!". '<br>';}
+//TO DO  -  put DB name in the sql injection
+//Specifieke columns opvragen uit DB
+$sql = 'SELECT 
+	studentinfo.Voornaam, 
+	studentinfo.Tussen, 
+	studentinfo.Achternaam, 
+	afspraken.ID, 
+	afspraken.Datum 
+        FROM 
+	studentinfo 
+	INNER JOIN afspraken ON afspraken.OV = studentInfo.OV 
+        ORDER BY 
+	datum ASC 
+        LIMIT 
+	5;';
+
+$result = $conStr->query($sql);
+
+if($result && $result->num_rows > 0){
+    //output data of each row
+    while($row = $result->fetch_assoc()){
+        echo "Voornaam: " . $row["Voornaam"].
+             " " . $row["Tussen"].
+             " " . $row["Achternaam"].
+             " - Datum: " . $row["Datum"]. "<br>";
+    }
+}else{
+    echo "0 results";
+}
