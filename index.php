38cb24fcbc3834b69cc50d655cc9cca67ee4aed7
DESKTOP-B87CD5K\Paco-pc <pablo_nicolaas@live.nl>
2017-03-06 13:29:41 +0100

    index aangepast de div tables zijn nu weldegelijk tables. en ik ben nog bezig met een responsive table die binnekort erop komt hij werkt nog niet .... ehm... helemaal zoals ik wil ;D

--- index.php
+++ index.php
@@ -34,6 +34,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
         <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="css.css">
+        <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.13/js/jquery.dataTables.min.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
         <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
         <!-- for Select dropdown menu-->
@@ -63,15 +65,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                             // Putting data from form into variables to be manipulated
                             $Datum = date("Y-m-d h:i:s");
                             $Opmerking = filter_input(INPUT_POST, 'formPostDescription');
-                            
+
                             //check if there is a student selected
                             if ($SelectedValue == "" || $SelectedValue == "all") {
                                 echo'Please select a student';
-                            }else{
-                                 $sqlExport = "INSERT INTO afspraken (OV, Datum, Opmerking) "
-                                    . "VALUES ('$SelectedValue', '$Datum', '$Opmerking')";
+                            } else {
+                                $sqlExport = "INSERT INTO afspraken (OV, Datum, Opmerking) "
+                                        . "VALUES ('$SelectedValue', '$Datum', '$Opmerking')";
 
-                            $result = $conStr->query($sqlExport);
+                                $result = $conStr->query($sqlExport);
                             }
                         }
                         ?>
@@ -154,7 +156,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
                                 $SelectedValue = filter_input(INPUT_POST, 'Select_Student');
                                 //temp
-                                echo 'here is your stuff: ' . $SelectedValue;
                                 ?>
                                 <!--export button-->
                                 <button type="button" class="btn btn-default" name="Exporteren" data-toggle="modal" data-target="#Export"><i class="fa fa-files-o" aria-hidden="true"></i>  Exporteren</button>
@@ -168,18 +169,23 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                     <div class="panel panel-default">
                         <div class="panel-heading">Aankomende gesprekken (laatste 5)</div>
                         <div class="panel-body">
-
-                            <div class="col-md-6">
-                                <dt>Naam</dt>
-                            </div>
-                            <div class="col-md-6">
-                                <dt>Datum</dt>
-                            </div>
-                            <br>
-                            <?php
+                            <table id="example" class="display" cellspacing="0" width="100%">
+                                <tr>
+                                    <td>
+                                        <div class="col-md-12">
+                                            <dt>Naam</dt>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div class="col-md-12">
+                                            <dt>Datum</dt>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <?php
 //TO DO  -  put DB name in the sql injection
 //Specifieke columns opvragen uit DB
-                            $sqlT5 = 'SELECT 
+                                $sqlT5 = 'SELECT 
                             studentinfo.Voornaam, 
                             studentinfo.Tussen, 
                             studentinfo.Achternaam, 
@@ -193,21 +199,32 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                             LIMIT 
                             5;';
 
-                            $result = $conStr->query($sqlT5);
+                                $result = $conStr->query($sqlT5);
 
-                            if ($result && $result->num_rows > 0) {
-                                //output data of each row
-                                while ($row = $result->fetch_assoc()) {
-                                    echo '<div class="col-md-6">' .
-                                    $row["Voornaam"] . " " .
-                                    $row["Tussen"] .
-                                    $row["Achternaam"] . '</div><div class="col-md-6">' .
-                                    $row["Datum"] . "</div>";
+                                if ($result && $result->num_rows > 0) {
+                                    //output data of each row
+                                    while ($row = $result->fetch_assoc()) {
+                                        echo '<tr>' . "<td>" .
+                                        '<div class="col-md-12">' .
+                                        $row["Voornaam"] . " " .
+                                        $row["Tussen"] .
+                                        $row["Achternaam"] .
+                                        '</div>' . '</td>' .
+                                        '<td>' .
+                                        '<div class="col-md-12">' .
+                                        $row["Datum"] . "</div>" . '</td>' . '</tr>';
+                                    }
+                                } else {
+                                    echo "0 results";
                                 }
-                            } else {
-                                echo "0 results";
-                            }
-                            ?>
+                                ?>
+                            </table>
+                            <script>
+                                $(document).ready(function () {
+                                    $('#example').DataTable();
+                                });
+                            </script>
+                            <br>
                         </div>
                     </div>
                 </div> 
@@ -218,64 +235,95 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             <div class="panel panel-default">
                 <div class="panel-heading"><dt>Informatie</dt></div>
                 <div class="panel-body">
-                    <div class="col-md-1">
-                        <dt>#</dt>
-                    </div>
-                    <div class="col-md-1">
-                        <dt>OV</dt>
-                    </div>
-                    <div class="col-md-2">
-                        <dt>Voornaam</dt>
-                    </div>
-                    <div class="col-md-1">
-                        <dt>Tussen</dt>
-                    </div>
-                    <div class="col-md-2">
-                        <dt>Achternaam</dt>
-                    </div>
-                    <div class="col-md-1">
-                        <dt>Jaar</dt>
-                    </div>
-                    <div class="col-md-1">
-                        <dt>Voortgang</dt>
-                    </div>
-                    <div class="col-md-2">
-                        <dt>Email</dt>
-                    </div>
-                    <div class="col-md-1">
-                        <dt>Foto</dt>
-                    </div>
+                    <table id="example" class="display" cellspacing="0" width="100%">
+                        <tr>
+                            <td>
+                                <div class="col-md-1">
+                                    <dt>#</dt>
+                                </div>
+                            </td>
+                            <td>
+                                <div class="col-md-1">
+                                    <dt>OV</dt>
+                                </div>
+                            </td>
+                            <td>
+                                <div class="col-md-2">
+                                    <dt>Voornaam</dt>
+                                </div>
+                            </td>
+                            <td>
+                                <div class="col-md-1">
+                                    <dt>Tussen</dt>
+                                </div>
+                            </td>
+                            <td>
+                                <div class="col-md-2">
+                                    <dt>Achternaam</dt>
+                                </div>
+                            </td>
+                            <td>
+                                <div class="col-md-1">
+                                    <dt>Jaar</dt>
+                                </div>
+                            </td>
+                            <td>
+                                <div class="col-md-1">
+                                    <dt>Voortgang</dt>
+                                </div>
+                            </td>
+                            <td>
+                                <div class="col-md-2">
+                                    <dt>Email</dt>
+                                </div>
+                            </td>
+                            <td>
+                                <div class="col-md-1">
+                                    <dt>Foto</dt>
+                                </div>
+                            </td>
+                        </tr>
+                        <!--                        voeg nu voor elke row de data in-->
+
+                        <?php
+                        // als er niks is geselecteerd willen we dat gewoon alles is geselecteerd                    
+                        //kijk of er iets is geselecteerd
+                        if ($SelectedValue == "" || $SelectedValue == 'all') {
+                            //geen sorteren gewoon alles selecteren
+                            $sqlPaneltitle = "SELECT ID, OV, Voornaam, Tussen, Achternaam, Klas, Email FROM studentinfo";
+                        } else if ($SelectedValue != "") {
+                            //sorteer op ov nummer
+                            $sqlPaneltitle = "SELECT ID, OV, Voornaam, Tussen, Achternaam, Klas, Email FROM studentinfo WHERE OV =" . $SelectedValue;
+                        }
+                        $resultPanelTitle = $conStr->query($sqlPaneltitle);
+                        if ($resultPanelTitle && $resultPanelTitle->num_rows > 0) {
+
+                            while ($row = $resultPanelTitle->fetch_assoc()) {
+                                echo '<tr>' .
+                                '<td><div class="col-md-1">' . $row["ID"] . "</div></td>" .
+                                '<td><div class="col-md-1">' . $row["OV"] . "</div></td>" .
+                                '<td><div class="col-md-2">' . $row["Voornaam"] . "</div></td>" .
+                                '<td><div class="col-md-1">' . $row["Tussen"] . "</div></td>" .
+                                '<td><div class="col-md-2">' . $row["Achternaam"] . "</div></td>" .
+                                '<td><div class="col-md-1">' . $row["Klas"] . "</div></td>" .
+                                '<td><div class="col-md-1"> </div></td>' .
+                                '<td><div class="col-md-2">' . $row["Email"] . '</div></td>' .
+                                '<td><div class="col-md-1"></div></td>' .
+                                '<td><div class="col-md-12"></div></td>' .
+                                '</tr>';
+                            }
+                        }
+                        ?>
+                    </table>
+                    <script>
+                        $(document).ready(function () {
+                            $('#example').DataTable();
+                        });
+                    </script>
                     <div class="col-md-12">
                         <!-- tijdelijk als tusenbarriere -->
                     </div>
-                    <?php
-                    // als er niks is geselecteerd willen we dat gewoon alles is geselecteerd                    
-                    //kijk of er iets is geselecteerd
-                    if ($SelectedValue == "" || $SelectedValue == 'all') {
-                        //geen sorteren gewoon alles selecteren
-                        $sqlPaneltitle = "SELECT ID, OV, Voornaam, Tussen, Achternaam, Klas, Email FROM studentinfo";
-                    } else if ($SelectedValue != "") {
-                        //sorteer op ov nummer
-                        $sqlPaneltitle = "SELECT ID, OV, Voornaam, Tussen, Achternaam, Klas, Email FROM studentinfo WHERE OV =" . $SelectedValue;
-                    }
-                    $resultPanelTitle = $conStr->query($sqlPaneltitle);
-                    if ($resultPanelTitle && $resultPanelTitle->num_rows > 0) {
 
-                        while ($row = $resultPanelTitle->fetch_assoc()) {
-                            echo '<div class="col-md-1">' . $row["ID"] . "</div>" .
-                            '<div class="col-md-1">' . $row["OV"] . "</div>" .
-                            '<div class="col-md-2">' . $row["Voornaam"] . "</div>" .
-                            '<div class="col-md-1">' . $row["Tussen"] . "</div>" .
-                            '<div class="col-md-2">' . $row["Achternaam"] . "</div>" .
-                            '<div class="col-md-1">' . $row["Klas"] . "</div>" .
-                            '<div class="col-md-1"> </div>' .
-                            '<div class="col-md-2">' . $row["Email"] . '</div>' .
-                            '<div class="col-md-1"></div>' .
-                            '<div class="col-md-12"></div>' .
-                            "<br>";
-                        }
-                    }
-                    ?>
                 </div>
             </div>
         </div>
